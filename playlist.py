9fc583022c71e0f865d4b7670b4fcd5d28d62df1
Didenko Ilia <didenko-ilia@users.noreply.github.com>
2018-04-07 19:12:17 +0200

    Moved static files to static folder, added mockup functions for app routes

--- playlist.py
+++ playlist.py
@@ -6,3 +6,17 @@ class Playlist:
 
     def __init__(self):
         self.list = queue.PriorityQueue()
+
+
+if __name__ == '__main__':
+    p = Playlist()
+    s1 = song.Song("1", 1);
+    s2 = song.Song("2", 2);
+    p.list.put(s1);
+    p.list.put(s2);
+    for s in p.list.queue:
+        print(s)
+
+    while not p.list.empty():
+        s = p.list.get()
+        print(s.songJson)
